60e190243b55af627d54d190d915e7f991165301
sajibislam105 <sajibislam105@gmail.com>
2022-02-17 04:48:10 +0600

    V1.0.1

---
+++ php-validation/RegistrationAction.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Registration Action</title>
+</head>
+<body>
+	<h1>Registration Successful</h1>
+	<p>Thank you for registering.</p>
+
+	<a href="registration.html">Go BacK
+</body>
+</html>
